9c83b85f38985475489059307a521422aba05109
Jordi <valdreck@gmail.com>
2021-05-09 22:22:32 +0200

    Counter example

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter.jsx'
 
 class App extends Component {
   render() {
     return (
     <div className="App">
       Hello App
-      <FirstComponent/>
+      <Counter/>
+      <Counter by={5}/>
+      <Counter by={10}/>
     </div>
   );
-  }
-}
-
-class FirstComponent extends Component {
-  render() {
-    return (
-      <div className="firstComponent">First Component</div>
-    );    
   }
 }
 
---
+++ frontend/todo-app/src/components/Counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css'
+
+class Counter extends Component {
+  // Define the initial state in a constructor
+  //state => counter 0
+  constructor(){
+    super();
+    this.state = {
+      counter : 0
+    }
+
+    this.increment = this.increment.bind(this);
+  }
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment} >+ {this.props.by}</button>
+        <span className="count">{this.state.counter}</span>        
+      </div>
+      
+    );    
+  }
+
+  increment(){    
+    this.setState({
+      counter: this.state.counter + this.props.by
+     });
+  }
+}
+
+Counter.defaultProps = {
+  by : 1
+}
+
+Counter.propTypes = {
+  by : PropTypes.number
+}
+
+export default Counter;
