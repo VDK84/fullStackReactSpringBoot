7d2e5c7d0421a2707812c666d09b2ec6539e1952
Jordi <valdreck@gmail.com>
2021-05-09 11:21:24 +0200

    initial frontend

---
+++ frontend/todo-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      Hello App
+      <FirstComponent/>
+    </div>
+  );
+  }
+}
+
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstComponent">First Component</div>
+    );    
+  }
+}
+
+export default App;
