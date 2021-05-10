936dcb64c583cb75c9ece7c6e1e218bad262e70a
Jordi <valdreck@gmail.com>
2021-05-10 21:38:52 +0200

    Add Todo List Add boostrap Add Header/Footer

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 import Counter from './components/Counter.jsx'
 import TodoApp from './components/todoApp/TodoApp.jsx'
 
@@ -8,8 +9,7 @@ import TodoApp from './components/todoApp/TodoApp.jsx'
 class App extends Component {
   render() {
     return (
-    <div className="App">
-      Hello App
+    <div className="App">      
       <TodoApp/>
     </div>
   );
--- frontend/todo-app/src/components/todoApp/TodoApp.jsx
+++ frontend/todo-app/src/components/todoApp/TodoApp.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 class TodoApp extends Component {
     render () {
         return (
             <div className="TodoApp">
                 <Router>
-                    <>
+                    <HeaderComponent/>                    
                         <Switch>
                             <Route path="/" exact component={LoginComponent}/>
                             <Route path="/login" component={LoginComponent}/>
                             <Route path="/welcome" component={WelcomeComponent}/>
+                            <Route path="/todos" component={ListTodosComponent}/>
                             <Route component={ErrorComponent}/>
-                        </Switch>
-                    </>
+                        </Switch>   
+                    <FooterComponent/>                 
                 </Router>                
             </div>
         );
@@ -41,12 +42,11 @@ class LoginComponent extends Component {
             <div>
                 {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
                 {this.state.showSucessMessage && <div>Login Successful Credentials</div>}
-                {!this.state.showSucessMessage && <div>
+                <div>
                     User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                     Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                     <button onClick={this.loginClicked}>Login</button>
-                </div>}
-                <WelcomeComponent/>
+                </div>           
             </div>
         );
     }
@@ -58,7 +58,7 @@ class LoginComponent extends Component {
     
     loginClicked(){
         if(this.state.username==='guest' && this.state.password==='guest'){
-            this.props.history.push("/welcome");
+            this.props.history.push(`/welcome/{this.state.username}`);
             this.setState({showSucessMessage:true});
             this.setState({hasLoginFailed:false});
         } else {
@@ -69,7 +69,72 @@ class LoginComponent extends Component {
 
 class WelcomeComponent extends Component{
     render () {
-        return <div>Welcome in TodoApp</div>
+        return <div>
+                Welcome X. You can manage your todos <Link to="/todos">here</Link>.
+            </div>
+    }
+}
+
+class HeaderComponent extends Component{
+    render(){
+        return (
+            <div>
+                Header <hr/>
+            </div>
+        )
+    }
+}
+
+class FooterComponent extends Component{
+    render(){
+        return (
+            <div>
+                 <hr/>Footer
+            </div>
+        )
+    }
+}
+
+class ListTodosComponent extends Component{
+constructor (props) {
+        super(props);
+        this.state = {
+            todos : 
+            [
+                {id: 1, description : 'Learn React', done:false, targetDate: new Date()},
+                {id: 2, description : 'Learn Spring', done:false, targetDate: new Date()},            
+                {id: 3, description : 'Learn Angular', done:false, targetDate: new Date()}            
+            ]
+        }
+    }
+    render () {
+        return (
+        <div>
+            <h1> List Todos</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>description</th>
+                        <th>Is Completed?</th>
+                        <th>Target Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.todos.map(
+                            todo =>
+                                <tr>                    
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
+                                </tr>
+                        )
+                    }                
+                </tbody>
+            </table>
+        </div>);
     }
 }
 
