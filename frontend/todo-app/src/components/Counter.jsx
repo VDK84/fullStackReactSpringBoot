06d0972ac7737a50ec3afb6b6fefb4e92cd8d078
Jordi <valdreck@gmail.com>
2021-05-10 11:49:06 +0200

    Add Reset button

--- frontend/todo-app/src/components/Counter.jsx
+++ frontend/todo-app/src/components/Counter.jsx
@@ -14,6 +14,7 @@ class Counter extends Component {
 
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   render() {
@@ -23,20 +24,27 @@ class Counter extends Component {
         <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}/>
         <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
         <span className="count">{this.state.counter}</span>
+        <div><button className="reset" onClick={this.reset}>Reset</button></div>
       </div>
     );
   }
 
   increment(by){    
-    this.setState({
-      counter: this.state.counter + by
-     });     
+    this.setState((prevState) => {
+      return {counter: prevState.counter + by}
+     });
   }
 
   decrement(by){    
+    this.setState((prevState) => {
+      return {counter: prevState.counter - by}
+     });
+  }
+
+  reset(){
     this.setState({
-      counter: this.state.counter - by
-     });     
+      counter: 0
+    });
   }
 }
 
