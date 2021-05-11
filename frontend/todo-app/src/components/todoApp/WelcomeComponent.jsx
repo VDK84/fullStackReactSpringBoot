6085d18c13ee60b69e0dad57ed90879386069040
Jordi <valdreck@gmail.com>
2021-05-11 22:37:59 +0200

    add axos add HelloWorldService that call spring REST

--- frontend/todo-app/src/components/todoApp/WelcomeComponent.jsx
+++ frontend/todo-app/src/components/todoApp/WelcomeComponent.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import HelloWorldService from '../../api/HelloWorldService.js';
 
 class WelcomeComponent extends Component{
+    constructor(props){
+        super(props);
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+        this.handleSuccesfulResponse = this.handleSuccesfulResponse.bind(this);
+        this.state = {
+            welcomeMessage: ''
+        }
+    }
     render () {
         return (
             <div>
@@ -9,8 +18,25 @@ class WelcomeComponent extends Component{
                 <div className="container">
                     Welcome {this.props.match.params.username}. You can manage your todos <Link to="/todos">here</Link>.
                 </div>
+                <div className="container">
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
             </div>
         );
+    }
+
+    retrieveWelcomeMessage(){
+        HelloWorldService.executeHelloWorldService()
+        .then(response => this.handleSuccesfulResponse(response))
+        .catch();
+    }
+
+    handleSuccesfulResponse(response){
+        this.setState({welcomeMessage: response.data});
     }
 }
 
